8f89508663ef71fb34d9e798cc6b2d84ace6b91a
SafinaAhsan <safinabagasra@gmail.com>
2024-01-16 11:44:25 +0500

    some bug fixes

--- src/app/component/about-me.tsx
+++ src/app/component/about-me.tsx
@@ -3,13 +3,13 @@
 const AboutMe = () => {
   return (
     <div className="px-6 sm:px-10">
-        <h1 className="text-2xl font-bold mt-8 sm:mt-12 hover:underline">ABOUT ME </h1>
-        <p className="mt-8 text-sm mb-2">👋 Hello there! I'm SAFINA AHSAN, a passionate coder on a thrilling journey in the world of programming. Over the past six months, I've delved into the realm of coding with unbridled enthusiasm, constantly seeking new challenges and opportunities to grow.</p>
-<p className="text-sm mt-4">📚 Currently, I'm a dedicated student at PIAIC Quarter 2, where I'm honing my skills and embracing the exciting challenges that the world of technology throws my way. Every line of code I write is a step forward in my quest for knowledge and mastery.</p>
+      <h1 className="text-2xl font-bold mt-8 sm:mt-12 hover:underline">ABOUT ME </h1>
+      <p className="mt-8 text-sm mb-2">👋 Hello there! I&apos;m SAFINA AHSAN, a passionate coder on a thrilling journey in the world of programming. Over the past six months, I&apos;ve delved into the realm of coding with unbridled enthusiasm, constantly seeking new challenges and opportunities to grow.</p>
+      <p className="text-sm mt-4">📚 Currently, I&apos;m a dedicated student at PIAIC Quarter 2, where I&apos;m honing my skills and embracing the exciting challenges that the world of technology throws my way. Every line of code I write is a step forward in my quest for knowledge and mastery.</p>
 
-<p className="text-sm mt-4">💻 My current focus is on web development, and my ambition knows no bounds. I'm on a mission to create a website that not only showcases my evolving skills but also mirrors the complexity and interactivity of platforms like Facebook. It's a thrilling journey of turning ideas into digital realities.</p>
-<p className="text-sm mt-4">🌐 Join me as I navigate the intricate web of coding, constantly inspired by the limitless possibilities that technology unfolds. Let's code the future together!</p> 
-</div>
+      <p className="text-sm mt-4">💻 My current focus is on web development, and my ambition knows no bounds. I&apos;m on a mission to create a website that not only showcases my evolving skills but also mirrors the complexity and interactivity of platforms like Facebook. It&apos;s a thrilling journey of turning ideas into digital realities.</p>
+      <p className="text-sm mt-4">🌐 Join me as I navigate the intricate web of coding, constantly inspired by the limitless possibilities that technology unfolds. Let&apos;s code the future together!</p>
+    </div>
   )
 }
 
